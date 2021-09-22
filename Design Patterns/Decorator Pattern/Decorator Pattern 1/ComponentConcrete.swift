0ef70d09dc93f5fc88bb25b13fcfa52917004931
R. Kukuh <rkukuh@gmail.com>
2021-09-22 16:31:49 +0700

    Add new files

---
+++ Design Patterns/Decorator Pattern/Decorator Pattern 1/ComponentConcrete.swift
@@ -0,0 +1,8 @@
+//
+//  ComponentConcrete.swift
+//  Design Patterns
+//
+//  Created by R. Kukuh on 22/09/21.
+//
+
+import Foundation
---
+++ Design Patterns/Decorator Pattern/Decorator Pattern 1/DecoratorConcreteA.swift
@@ -0,0 +1,8 @@
+//
+//  DecoratorConcreteA.swift
+//  Design Patterns
+//
+//  Created by R. Kukuh on 22/09/21.
+//
+
+import Foundation
